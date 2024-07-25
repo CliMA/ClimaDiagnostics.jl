d339424473928be978ca3c9947cc32b3ee3e8eab
Gabriele Bozzola <sbozzolator@gmail.com>
2024-07-24 23:47:42 -0700

    Add ExplicitImports
    ExplicitImports helps keeping functions well qualified and unambiguous

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,6 +1,7 @@
 using Test
 using ClimaDiagnostics
 using Aqua
+import ExplicitImports
 
 @testset "Aqua tests" begin
     Aqua.test_undefined_exports(ClimaDiagnostics)
@@ -8,4 +9,37 @@ using Aqua
     Aqua.test_deps_compat(ClimaDiagnostics)
     Aqua.detect_ambiguities(ClimaDiagnostics; recursive = true)
     Aqua.test_piracies(ClimaDiagnostics)
+end
+
+@testset "Explicit Imports" begin
+    @test isnothing(ExplicitImports.check_no_implicit_imports(ClimaDiagnostics))
+
+    # We import some variables to bring them to the top level, so that they are
+    # easier to use in user packages. These are technically stale imports, so we
+    # have to ignore them
+
+    ignore = (
+        :DiagnosticVariable,
+        :DivisorSchedule,
+        :ScheduledDiagnostic,
+        :average_pre_output_hook!,
+    )
+    @test isnothing(
+        ExplicitImports.check_no_stale_explicit_imports(
+            ClimaDiagnostics;
+            ignore,
+        ),
+    )
+
+    # ClimaCore idiosyncrasies
+    ignore = (:HDF5, :topology, :vertical_topology)
+    @test isnothing(
+        ExplicitImports.check_all_qualified_accesses_via_owners(
+            ClimaDiagnostics;
+            ignore,
+        ),
+    )
+    @test isnothing(
+        ExplicitImports.check_no_self_qualified_accesses(ClimaDiagnostics),
+    )
 end
