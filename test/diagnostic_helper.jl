5a92ea9c7c15211c22f0540068dbe6a243208f50
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-12-09 15:31:41 -0800

    Refactor computing fields

--- test/diagnostic_helper.jl
+++ test/diagnostic_helper.jl
@@ -42,3 +42,54 @@ import ClimaDiagnostics:
         )
     end
 end
+
+@testset "Compute fields" begin
+    var_compute_in_place = DiagnosticVariables.DiagnosticVariable(;
+        short_name = "my",
+        long_name = "My test",
+        standard_name = "my_test",
+        units = "m",
+        comments = "It works!",
+        compute! = (out, u, p, t) -> isnothing(out) ? 1 : (out .= 1),
+    )
+    var_compute = DiagnosticVariables.DiagnosticVariable(;
+        short_name = "my",
+        long_name = "My test",
+        standard_name = "my_test",
+        units = "m",
+        comments = "It works!",
+        compute = (u, p, t) -> 1,
+    )
+    var_lazy_compute = DiagnosticVariables.DiagnosticVariable(;
+        short_name = "my",
+        long_name = "My test",
+        standard_name = "my_test",
+        units = "m",
+        comments = "It works!",
+        compute = (u, p, t) -> Base.Broadcast.broadcasted(+, 0, 1),
+    )
+
+    function create_diag(variable)
+        return ScheduledDiagnostics.ScheduledDiagnostic(;
+            variable = variable,
+            output_writer = Writers.DummyWriter(),
+            output_schedule_func = Schedules.EveryDtSchedule(5),
+            compute_schedule_func = Schedules.EveryDtSchedule(5),
+        )
+    end
+
+    u = nothing
+    p = nothing
+    t = nothing
+    vars = (var_compute_in_place, var_compute, var_lazy_compute)
+
+    for var in vars
+        # Test allocating version
+        @test isone(ClimaDiagnostics.compute_field(create_diag(var), u, p, t))
+
+        # Test non allocating version
+        dest = [0]
+        ClimaDiagnostics.compute_field!(dest, create_diag(var), u, p, t)
+        @test isone(first(dest))
+    end
+end
